3c16380503d568381b2ca341d2321a634a5feccb
brbbrb <85380887+brbbrb@users.noreply.github.com>
2021-07-06 14:26:38 -0400

    Stuck on PyBank, moving on to PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,13 @@
-#Budget Data
+# Budget Data
 
-#Import the os module
-#This will allow us to create file path to budget data
+# Import the os module
+# This will allow us to create file path to budget data
 import os
 
-#Import the module for reading csv files
+# Import the module for reading csv files
 import csv
 
-#os.path.join would not work, so I used this
+# os.path.join would not work, so I used this
 csvpath = '/Users/bradleybarker/Documents/GT_Boot_Camp/GT_Homework/Week_03_Python/python-challenge/PyBank/Resources/budget_data.csv'
 
 with open(csvpath) as csvfile:
@@ -15,13 +15,36 @@ with open(csvpath) as csvfile:
     # Use CSV reader to specify delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
+    #print(csvreader)
 
     # Read the header row first
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    # print(f"CSV Header: {csv_header}")
+
+    # Define last month number
+    last_month = 0 
 
     # Read each row of data after the header
     for row in csvreader:
-        print(row)
+        month = row[0]
+        profit = row[1]
+        #print(month)
+        #print(profit)
+        # Assign columns to type classes
 
+        month_string = str(month)
+        #print(type(month_string))
+
+        profit_integer = int(profit)
+        #print(type(profit_integer))
+
+    # Count total number of months in dataset
+    month_count = len(month)
+    print(month_count)
+
+    # Determine total net profit/loss over entire period
+    net_profit = sum(profit)
+    print(net_profit)
+
+        # Determine change in profit
+        #change = 
