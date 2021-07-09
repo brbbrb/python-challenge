682442850ee0d1cb51f1dbb1d562f3427f83f717
brbbrb <85380887+brbbrb@users.noreply.github.com>
2021-07-09 19:45:59 -0400

    Made many changes, but lots of bugs

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,6 +10,8 @@ import csv
 # os.path.join would not work, so I used this
 csvpath = '/Users/bradleybarker/Documents/GT_Boot_Camp/GT_Homework/Week_03_Python/python-challenge/PyBank/Resources/budget_data.csv'
 
+
+
 with open(csvpath) as csvfile:
 
     # Use CSV reader to specify delimiter and variable that holds contents
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,9 +9,9 @@ import csv
 
 # os.path.join would not work, so I used this
 csvpath = '/Users/bradleybarker/Documents/GT_Boot_Camp/GT_Homework/Week_03_Python/python-challenge/PyPoll/Resources/election_data.csv'
+
 with open(csvpath) as csvfile:
 
     # Use CSV reader to specify delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-
     print(csvreader)
