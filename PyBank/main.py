cc0abe4780b3a674635b53de01df7f80842b6432
brbbrb <85380887+brbbrb@users.noreply.github.com>
2021-07-09 23:28:41 -0400

    Finished PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,4 @@
-# PyBank Challenge
+# PyBank
 
 # Import the os module
 # This will allow us to create file path to budget data
@@ -7,44 +7,80 @@ import os
 # Import the module for reading csv files
 import csv
 
-# os.path.join would not work, so I used this
+# os.path.join would not work, so I had to use full CPU location
+#csvpath = os.path.join('Resources','budget_data.csv')
 csvpath = '/Users/bradleybarker/Documents/GT_Boot_Camp/GT_Homework/Week_03_Python/python-challenge/PyBank/Resources/budget_data.csv'
 
+# Create lists
+months = []
+profits = []
+monthly_change = []
 
-
+# Reading using CSV module
 with open(csvpath) as csvfile:
 
-    # Use CSV reader to specify delimiter and variable that holds contents
+    # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    # Read the header row first
+    # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
-    # Define last month number
-    last_month = 0 
-
+    # Set last month to 0
+    last_month = 0
+    
     # Read each row of data after the header
     for row in csvreader:
-        month = row[0]
-        profit = row[1]
-        #print(month)
-        #print(profit)
-        # Assign columns to type classes
+        month = str(row[0])
+        profit = int(row[1])
+        
+        #print(row)
+        
+        # Add data to month, profit, and montly_change lists
+        months.append(month)
+        profits.append(profit)
+        change = profit - last_month
+        monthly_change.append(change)
+        last_month = profit
 
-        month_string = str(month)
-        #print(type(month_string))
+#Determine final calculations    
+    
+total_months = len(months)
+#print(total_months)
 
-        profit_integer = int(profit)
-        #print(type(profit_integer))
+total_profit = sum(profits)
+#print(total_profit)
 
-    # Count total number of months in dataset
-    month_count = len(month)
-    print(month_count)
+avg = round((sum(monthly_change)/len(monthly_change)),2)
+#print(avg)
 
-    # Determine total net profit/loss over entire period
-    net_profit = sum(profit)
-    print(net_profit)
+maxprofit = max(profits)
+#print(maxprofit)
 
-        # Determine change in profit
-        #change = 
+minprofit = min(profits)
+#print(minprofit)
+
+maxrow = profits.index(maxprofit)
+#print(maxrow)
+
+minrow = profits.index(minprofit)
+#print(minrow)
+
+maxmonth = str(months[maxrow])
+#print(maxmonth)
+
+minmonth = str(months[minrow])
+#print(minmonth)
+
+# Message
+with open ("pybank_financial_analysis","w")as file:
+    file.write("Financial Analysis" "\n")
+    file.write("------------------------" "\n")
+    file.write(f'Total Months: {total_months}' "\n")
+    file.write(f'Total: ${total_profit}' "\n")
+    file.write(f'Average Change: ${avg}' "\n")
+    file.write(f'Greatest Increase in Profits: {maxmonth} (${maxprofit})' "\n")
+    file.write(f'Greatest Decrease in Profits: {minmonth} (${minprofit})')
+
+file = open("pybank_financial_analysis","r")
+print(file.read())
