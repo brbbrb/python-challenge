9564682207d0d6ca6d33b0ad3c85ed339ffeb7f4
brbbrb <85380887+brbbrb@users.noreply.github.com>
2021-07-11 15:20:54 -0400

    Determined percentages and winner

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,11 +7,20 @@ import os
 # Import the module for reading csv files
 import csv
 
+# Create lists
+#voter_ids = 
+#counties = 
+#candidates = 
+
 # os.path.join would not work, so I used this
 csvpath = '/Users/bradleybarker/Documents/GT_Boot_Camp/GT_Homework/Week_03_Python/python-challenge/PyPoll/Resources/election_data.csv'
+#path=os.path.join("Resources","election_data.csv")
+print(csvpath)
 
 with open(csvpath) as csvfile:
 
     # Use CSV reader to specify delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
+    csv_header = next(csvreader)
+    for row in csvreader:
+        print row
